adc352d7eeaf457abd0a49d63885d44b42e216a7
lukaendermen <lukamecxovrishvili14@gamil.com>
2024-08-22 11:24:18 +0400

    second commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,47 @@
+list1 = ["luka"]
+list2 = ["gio","dato"]
+list3 = ["nika","saba","zuka"]
+list4 = ["temo","deme","data","jaba"]
+list5 = ["lika","lana","lizi","ana","nino"]
+
+print (list1[0])
+print (list1[-1])
+
+print (list2[0])
+print (list2[1])
+print (list2[-1])
+print (list2[-2])
+
+print (list3[0])
+print (list3[1])
+print (list3[2])
+print (list3[-1])
+print (list3[-2])
+print (list3[-3])
+
+print (list4[0])
+print (list4[1])
+print (list4[2])
+print (list4[3])
+print (list4[-1])
+print (list4[-2])
+print (list4[-3])
+print (list4[-4])
+
+print (list5[0])
+print (list5[1])
+print (list5[2])
+print (list5[3])
+print (list5[4])
+print (list5[5])
+print (list5[-1])
+print (list5[-2])
+print (list5[-3])
+print (list5[-4])
+print (list5[-5])
+
+
+
+
+
+
---
+++ level 14/classwork/classwork2.py
@@ -0,0 +1,6 @@
+listnumber = [1,2,3,4,5,6,7,8,9,10]
+
+number1 = print (listnumber[0+9])
+numder2 = print (listnumber[1-8])
+numder3 = print (listnumber[4+6-3 ])
+
